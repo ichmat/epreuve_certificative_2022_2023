b0c681e2167eff197c44d9b0056e1cc8a12180d3
cedric guillemin <cedricguilleminpro@gmail.com>
2023-07-03 13:51:42 +0200

    Add content for construction in database

--- AppCore/Services/NecessaryData.cs
+++ AppCore/Services/NecessaryData.cs
@@ -272,8 +272,138 @@ namespace AppCore.Services
                 }, new[] {
                     800
                 })
-                .AddReparationRessources(GetRessource(RESSOURCE.BOIS), 20, 1.5f, 1200),
-        };
+   .AddReparationRessources(GetRessource(RESSOURCE.BOIS), 20, 1.5f, 1200),
+
+                        ConstructionInfoSchema.CreateDef(3, "Barbelé", 120, TypeConstruction.DEFENSE, 3600, 1.4f, 20)
+                .AddCreationRessources(GetRessource(RESSOURCE.FERRAILLE),100)
+                .AddLevel(new[] { // niveau 2
+                    GetRessource(RESSOURCE.FERRAILLE)
+                }, new[] {
+                    110
+                })
+                .AddLevel(new[] { // niveau 3
+                    GetRessource(RESSOURCE.FERRAILLE)
+                }, new[] {
+                    160,
+                })
+                .AddLevel(new[] { // niveau 4
+                    GetRessource(RESSOURCE.FERRAILLE)
+                }, new[] {
+                    200,
+                })
+                .AddLevel(new[] { // niveau 5
+                    GetRessource(RESSOURCE.FERRAILLE)
+                }, new[] {
+                    150,
+                })
+                .AddLevel(new[] { // niveau 6
+                    GetRessource(RESSOURCE.FERRAILLE)
+                }, new[] {
+                    800
+                })
+   .AddReparationRessources(GetRessource(RESSOURCE.BOIS), 20, 1.5f, 1200),
+
+                ConstructionInfoSchema.CreateDef(4, "Tour de Garde", 150, TypeConstruction.DEFENSE, 4800, 1.8f, 30)
+    .AddCreationRessources(GetRessource(RESSOURCE.BOIS), 200)
+    .AddCreationRessources(GetRessource(RESSOURCE.FERRAILLE), 150)
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS), GetRessource(RESSOURCE.FERRAILLE) }, new[] { 220, 50 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS), GetRessource(RESSOURCE.FERRAILLE) }, new[] { 300, 120 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS), GetRessource(RESSOURCE.FERRAILLE) }, new[] { 400, 250 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS), GetRessource(RESSOURCE.FERRAILLE) }, new[] { 550, 400 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE) }, new[] { 1200 })
+    .AddReparationRessources(GetRessource(RESSOURCE.BOIS), 30, 1.6f, 1800),
+
+    ConstructionInfoSchema.CreateDef(5, "Canon", 400, TypeConstruction.DEFENSE, 14400, 2.5f, 150)
+        .AddCreationRessources(GetRessource(RESSOURCE.FERRAILLE), 300)
+        .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 400, 200 })
+        .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 600, 400 })
+        .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 900, 600 })
+        .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 1200, 1000 })
+        .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE) }, new[] { 2000 })
+        .AddReparationRessources(GetRessource(RESSOURCE.BOIS), 50, 2.3f, 200),
+
+
+    ConstructionInfoSchema.CreateDef(6, "Centrale électrique", 200, TypeConstruction.ENERGIE, 6000, 1.8f, 30)
+        .AddCreationRessources(GetRessource(RESSOURCE.FERRAILLE), 100)
+        .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 150, 80 })
+        .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 200, 120 })
+        .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 250, 160 })
+        .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 520, 450 })
+        .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE) }, new[] { 1000 })
+        .AddReparationRessources(GetRessource(RESSOURCE.FERRAILLE), 20,  1.5f, 100),
+
+ConstructionInfoSchema.CreateDef(7, "Ferme", 150, TypeConstruction.NOURRITURE, 4800, 1.5f, 25)
+    .AddCreationRessources(GetRessource(RESSOURCE.BOIS), 80)
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS) }, new[] { 100 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS) }, new[] { 200 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS) }, new[] { 350 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS) }, new[] { 450 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS), GetRessource(RESSOURCE.FERRAILLE) }, new[] { 150, 50 })
+    .AddReparationRessources(GetRessource(RESSOURCE.BOIS), 15, 1.2f, 100),
+
+ConstructionInfoSchema.CreateDef(8, "Scierie", 180, TypeConstruction.ENERGIE, 5400, 1.7f, 35)
+    .AddCreationRessources(GetRessource(RESSOURCE.FERRAILLE), 120)
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE),GetRessource(RESSOURCE.BOIS) }, new[] { 150, 150 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE),GetRessource(RESSOURCE.BOIS) }, new[] { 180, 220 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 220, 80 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 300, 320 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 450, 500 })
+    .AddReparationRessources(GetRessource(RESSOURCE.FERRAILLE), 25, 1.3f, 100),
+
+                ConstructionInfoSchema.CreateDef(9, "Usine de traitement d'eau", 200, TypeConstruction.EAU, 6000, 1.8f, 30)
+    .AddCreationRessources(GetRessource(RESSOURCE.FERRAILLE), 150)
+    .AddCreationRessources(GetRessource(RESSOURCE.BOIS), 100)
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 180, 80 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 220, 120 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 260, 160 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 300, 250 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 450, 500 })
+    .AddReparationRessources(GetRessource(RESSOURCE.FERRAILLE), 30, 1.2f, 300)
+    .AddReparationRessources(GetRessource(RESSOURCE.BOIS), 20, 1.5f, 1200),
+
+                ConstructionInfoSchema.CreateDef(10, "Quartier résidentiel", 250, TypeConstruction.HABITATION, 7200, 2.0f, 40)
+    .AddCreationRessources(GetRessource(RESSOURCE.BOIS), 200)
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS) }, new[] { 250 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS) }, new[] { 300 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS) }, new[] { 420 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS) }, new[] { 500 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS) }, new[] { 600 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS), GetRessource(RESSOURCE.FERRAILLE) }, new[] { 600, 600 })
+    .AddReparationRessources(GetRessource(RESSOURCE.BOIS), 40, 1.3f, 400),
+
+                ConstructionInfoSchema.CreateDef(11, "Fonderie", 180, TypeConstruction.ENERGIE, 5400, 1.7f, 35)
+    .AddCreationRessources(GetRessource(RESSOURCE.BOIS), 120)
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS) }, new[] { 150 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS) }, new[] { 180 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS), GetRessource(RESSOURCE.FERRAILLE) }, new[] { 220, 80 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS), GetRessource(RESSOURCE.FERRAILLE) }, new[] { 280, 120 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.BOIS), GetRessource(RESSOURCE.FERRAILLE) }, new[] { 350, 200 })
+    .AddReparationRessources(GetRessource(RESSOURCE.BOIS), 25, 1.3f, 350),
+
+                ConstructionInfoSchema.CreateDef(12, "Caserne", 220, TypeConstruction.ATTAQUANT, 6600, 1.9f, 30)
+    .AddCreationRessources(GetRessource(RESSOURCE.FERRAILLE), 180)
+    .AddCreationRessources(GetRessource(RESSOURCE.BOIS), 100)
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 200, 80 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 240, 120 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 300, 160 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 350, 200 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 450, 300 })
+    .AddReparationRessources(GetRessource(RESSOURCE.FERRAILLE), 35, 1.2f, 300)
+    .AddReparationRessources(GetRessource(RESSOURCE.BOIS), 25, 1.5f, 300),
+
+                ConstructionInfoSchema.CreateDef(13, "Théâtre", 200, TypeConstruction.DIVERTISSEMENT, 6000, 1.8f, 40)
+    .AddCreationRessources(GetRessource(RESSOURCE.BOIS), 150)
+    .AddCreationRessources(GetRessource(RESSOURCE.FERRAILLE), 100)
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 200, 80 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 240, 120 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 300, 160 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 350, 200 })
+    .AddLevel(new[] { GetRessource(RESSOURCE.FERRAILLE), GetRessource(RESSOURCE.BOIS) }, new[] { 450, 300 })
+    .AddReparationRessources(GetRessource(RESSOURCE.BOIS), 30, 1.2f, 300)
+    .AddReparationRessources(GetRessource(RESSOURCE.FERRAILLE), 20, 1.5f, 300)
+
+
+    };
 
         #endregion
     }
